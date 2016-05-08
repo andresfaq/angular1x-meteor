b8c7269ddd1fba5beb3349d6eead395afeed40f1
andresfaq <andres.artunduaga@softpowering.com>
2016-05-07 20:00:21 -0500

    fixing gitignore

---
+++ socially/client/main.js
@@ -0,0 +1,5 @@
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+import { name as Socially } from '../imports/ui/components/socially/socially';
+
+
---
+++ socially/imports/ui/components/partiesList/partiesList.js
@@ -0,0 +1,35 @@
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+import './partiesList.html';
+import { Parties } from '../../../api/parties';
+import { name as PartyAdd } from '../partyAdd/partyAdd';
+import { name as PartyRemove } from '../partyRemove/partyRemove';
+
+class PartiesList {
+    constructor($scope, $reactive) {
+        'ngInject';
+
+        $reactive(this).attach($scope);
+
+        this.helpers({
+            parties() {
+                return Parties.find({});
+            }
+        });
+    }
+}
+
+const name = 'partiesList';
+
+// create a module
+export default angular.module(name, [
+    angularMeteor,
+    PartyAdd,
+    PartyRemove
+]).component(name, {
+    templateUrl: `imports/ui/components/${name}/${name}.html`,
+    controllerAs: name,
+    controller: PartiesList
+});
+
+
