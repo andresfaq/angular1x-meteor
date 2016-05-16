0f05e7c20fcb94153b2e98b963db10d0784c0101
andresfaq <andres.artunduaga@softpowering.com>
2016-05-16 18:53:10 -0500

    Stage 5 - Routing complete

---
+++ socially/imports/ui/components/navigation/navigation.js
@@ -0,0 +1,16 @@
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+import template from './navigation.html';
+
+const name = 'navigation';
+
+// create a module
+
+export default angular.module(name, [
+    angularMeteor
+]).component(name, {
+    // template,
+    templateUrl: `imports/ui/components/${name}/${name}.html`,
+    controllerAs: name
+});
+
---
+++ socially/imports/ui/components/partyAdd/partyAdd.js
@@ -0,0 +1,33 @@
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+import { Parties } from '../../../api/parties';
+import './partyAdd.html';
+
+class PartyAdd {
+
+    constructor() {
+        this.party = {};
+    }
+
+    submit() {
+        Parties.insert(this.party);
+        this.reset();
+    }
+
+    reset() {
+        this.party = {};
+    }
+    
+}
+
+const name = 'partyAdd';
+
+// create a module
+export default angular.module(name, [
+    angularMeteor
+]).component(name, {
+    templateUrl: `imports/ui/components/${name}/${name}.html`,
+    controllerAs: name,
+    controller: PartyAdd
+});
+
